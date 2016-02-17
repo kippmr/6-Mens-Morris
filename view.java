a772ba457210c46b522802ca0c32e29fff545294
Ben Miller <benjaminmiller229@gmail.com>
2016-02-17 03:46:50 -0500

    Update view.java

--- view.java
+++ view.java
@@ -1,3 +1,4 @@
+// import statements
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.*;
@@ -8,8 +9,10 @@ import javafx.stage.*;
 public class viewer extends Application{
 	Stage window;
 	Scene mainScene,boardScene,creditScene;
-	// Creates buttons
-	Button button,button2,button3, button4, button5, button6;
+	// Menu Buttons
+	Button onePlayer,twoPlayers,credits, exitButton, quitButton, returnButton;
+	// Buttons for controller logic
+	Button reset,check,red,blue;
 	
 	static Boolean confirm;
 	
@@ -38,49 +41,65 @@ public class viewer extends Application{
 		Label label3 = new Label("Model: William Tran\nView: Ben Miller\nController: Matt Kipp");
 		
 		// Creating button
-		button = new Button();
-		button.setText("1 Player");
-		button.setOnAction(e -> window.setScene(boardScene));
+		onePlayer= new Button();
+		onePlayer.setText("1 Player");
+		onePlayer.setOnAction(e -> window.setScene(boardScene));
 		
 		// 2 Players option
-		button2 = new Button();
-		button2.setText("2 Players");
-		button2.setOnAction(e -> window.setScene(boardScene));
+		twoPlayers = new Button();
+		twoPlayers.setText("2 Players");
+		twoPlayers.setOnAction(e -> window.setScene(boardScene));
 		
 		// Credits Option
-		button3 = new Button();
-		button3.setText("Credits");
-		button3.setOnAction(e -> window.setScene(creditScene));
+		credits = new Button();
+		credits.setText("Credits");
+		credits.setOnAction(e -> window.setScene(creditScene));
 		
 		// Exit program button / handler
-		button4 = new Button();
-		button4.setText("Exit");
-		button4.setOnAction(e -> closeProgram());
+		exitButton = new Button();
+		exitButton.setText("Exit");
+		exitButton.setOnAction(e -> closeProgram());
 		
 		// Return to main menu button
-		button5 = new Button();
-		button5.setText("Quit");
-		button5.setOnAction(e -> window.setScene(mainScene));
+		quitButton = new Button();
+		quitButton.setText("Quit");
+		quitButton.setOnAction(e -> window.setScene(mainScene));
 		
 		// Returns from credit screen to main screen
-		button6 = new Button();
-		button6.setText("Return");
-		button6.setOnAction(e -> window.setScene(mainScene));
+		returnButton = new Button();
+		returnButton.setText("Return");
+		returnButton.setOnAction(e -> window.setScene(mainScene));
+		
+		red = new Button();
+		red.setText("R");
+		//red.setOnAction(e -> Logic for setting a red piece, handled by controller)
+		
+		blue = new Button();
+		blue.setText("B");
+		//blue.setOnAction(e -> Logic for setting a blue piece, handled by controller)
+		
+		check = new Button();
+		check.setText("Check");
+		//check.setOnAction(e -> Logic for checking if the pieces are legal, handled by controller)
+		
+		reset = new Button();
+		reset.setText("Reset");
+		//reset.setOnAction(e -> Resets the game pieces, handled by controller)
 		
 		// Main Screen layout
 		VBox layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1,button,button2,button3,button4);
-		mainScene = new Scene(layout1,450,300);
+		layout1.getChildren().addAll(label1,onePlayer,twoPlayers,credits,exitButton);
+		mainScene = new Scene(layout1,550,400);
 		
 		// Board game layout
 		VBox layout2 = new VBox(20);
-		layout2.getChildren().addAll(label2,button5);
-		boardScene = new Scene(layout2,450,300);
+		layout2.getChildren().addAll(label2,red,blue,reset,check,quitButton);
+		boardScene = new Scene(layout2,550,400);
 		
 		// Credit Screen layout
 		VBox layout3 = new VBox(20);
-		layout3.getChildren().addAll(label3,button6);
-		creditScene = new Scene(layout3,450,300);
+		layout3.getChildren().addAll(label3,returnButton);
+		creditScene = new Scene(layout3,550,400);
 		
 		// Center the buttons
 		layout1.setAlignment(Pos.CENTER);
@@ -98,3 +117,4 @@ public class viewer extends Application{
 		System.exit(0);}
 	}
 }
+
