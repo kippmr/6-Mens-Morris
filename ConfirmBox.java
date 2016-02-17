4abd7a07b5445ff1a8a2b987b6a7e95439e2f0b6
Ben Miller <benjaminmiller229@gmail.com>
2016-02-17 03:30:28 -0500

    Create ConfirmBox.java

---
+++ ConfirmBox.java
@@ -0,0 +1,42 @@
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+
+public class ConfirmBox {
+
+	static boolean confirm;
+	
+	public static boolean display(String title, String message){
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);
+		Label label = new Label();
+		label.setText(message);
+		
+		// Create yes and no buttons
+		Button yesButton = new Button("Yes");
+		Button noButton = new Button("No");
+		
+		yesButton.setOnAction(e ->{
+			confirm = true;
+			window.close();
+		});
+		
+		noButton.setOnAction(e ->{
+			confirm = false;
+			window.close();
+		});
+		
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label,yesButton,noButton);
+		layout.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		window.showAndWait();
+		
+		return confirm;
+	}
+}
