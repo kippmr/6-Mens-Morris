eadfc62581e81d951daac77f90f162e233941f6b
Ben Miller <benjaminmiller229@gmail.com>
2016-03-21 19:02:45 -0400

    Update ConfirmBox.java

--- 6_Mens_Morris/src/view/ConfirmBox.java
+++ 6_Mens_Morris/src/view/ConfirmBox.java
@@ -1,4 +1,5 @@
 package view;
+// import statements
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -6,9 +7,16 @@ import javafx.stage.*;
 import javafx.geometry.*;
 
 public class ConfirmBox {
-
+	
+	// Boolean value whether the user does want to exit the program or not
 	static boolean confirm;
 	
+	/**
+	 * Creates a window that displays whether the user would like to quit or not with Y/N buttons for choosing
+	 * @param title - string value of the title of the display box
+	 * @param message - message being sent to the user in the display box
+	 * @return - returns the users response as a boolean: Yes or No to closing the program completely
+	 */
 	public static boolean display(String title, String message){
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
@@ -21,16 +29,19 @@ public class ConfirmBox {
 		Button yesButton = new Button("Yes");
 		Button noButton = new Button("No");
 		
+		// If the user does want to exit, close the program completely
 		yesButton.setOnAction(e ->{
 			confirm = true;
 			window.close();
 		});
 		
+		// If the user decides they do not want to exit, exit the confirm box class and return False
 		noButton.setOnAction(e ->{
 			confirm = false;
 			window.close();
 		});
 		
+		// The layout of the confirm exit box
 		VBox layout = new VBox(10);
 		layout.getChildren().addAll(label,yesButton,noButton);
 		layout.setAlignment(Pos.CENTER);
@@ -38,6 +49,7 @@ public class ConfirmBox {
 		window.setScene(scene);
 		window.showAndWait();
 		
+		// returns the users response Y/N
 		return confirm;
 	}
 }
